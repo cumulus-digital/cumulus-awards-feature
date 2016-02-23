b0435e9b1597ccc213f71f6752b3b95dca96d632
Daniel Vena <deaconvena+github@gmail.com>
2016-02-23 15:26:35 -0500

    💥 Initial commit

---
+++ dist/js/base-min.js
@@ -0,0 +1,2 @@
+!function($,e,n){e.installGoogleAnalytics=function(n){n&&(!function(e,n,t,a,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=n.createElement(t),s=n.getElementsByTagName(t)[0],o.async=1,o.src=a,s.parentNode.insertBefore(o,s)}(e,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create",n,"auto",{name:"contestTracker"}),ga("contestTracker.send","pageview"))}}(jQuery,window);
+//# sourceMappingURL=./base-min.js.map
