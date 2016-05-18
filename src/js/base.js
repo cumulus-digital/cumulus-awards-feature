c9071d8342c184cb6fafb58c7420d626b6c15539
Daniel Vena <deaconvena+github@gmail.com>
2016-05-18 16:31:39 -0400

    Generates tracker name from page title when installing GA

--- src/js/base.js
+++ src/js/base.js
@@ -4,7 +4,7 @@
 	 * Installs Google Analytics
 	 * @param  {string} id GA Property ID
 	 */
-	window.installGoogleAnalytics = function(id) {
+	window.installGoogleAnalytics = function(id, name) {
 		if (! id) {
 			return;
 		}
@@ -12,13 +12,31 @@
 		/* jshint ignore:start */
 		/*jsl:ignore* /
 		/*ignore jslint start*/
+		if ( ! name) {
+			function hashIt(str) {
+			  var hash = 5381,
+			      i    = str.length
+
+			  while(i)
+			    hash = (hash * 33) ^ str.charCodeAt(--i)
+
+			  /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+			   * integers. Since we want the results to be always positive, convert the
+			   * signed int to an unsigned by doing an unsigned bitshift. */
+			  return hash >>> 0;
+			}
+			name = 'fT' + hashIt(window.self.document.title);
+		} else {
+			name = 'fT' + name;
+		}
+
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', id, 'auto', {'name': 'contestTracker'});
-		ga('contestTracker.send', 'pageview');
+		ga('create', id, 'auto', {'name': name});
+		ga(name + '.send', 'pageview');
 		/*ignore jslint end*/
 		/*jsl:end */
 		/* jshint ignore:end */
