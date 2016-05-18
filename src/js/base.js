518535c1d1bfc62141712949baf63e175ed55582
Daniel Vena <deaconvena+github@gmail.com>
2016-05-18 16:39:03 -0400

    Previous commit failed to compile

--- src/js/base.js
+++ src/js/base.js
@@ -1,6 +1,6 @@
 (function($, window, undefined){
 	
-	/**
+		/**
 	 * Installs Google Analytics
 	 * @param  {string} id GA Property ID
 	 */
@@ -12,24 +12,33 @@
 		/* jshint ignore:start */
 		/*jsl:ignore* /
 		/*ignore jslint start*/
-		if ( ! name) {
-			function hashIt(str) {
-			  var hash = 5381,
-			      i    = str.length
+		/* adapted from https://github.com/darkskyapp/string-hash */
+		function hashIt(str) {
+			var hash = 5381,
+			i = str.length;
 
-			  while(i)
-			    hash = (hash * 33) ^ str.charCodeAt(--i)
-
-			  /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
-			   * integers. Since we want the results to be always positive, convert the
-			   * signed int to an unsigned by doing an unsigned bitshift. */
-			  return hash >>> 0;
+			while(i) {
+				hash = (hash * 33) ^ str.charCodeAt(--i);
 			}
+
+			/* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+			* integers. Since we want the results to be always positive, convert the
+			* signed int to an unsigned by doing an unsigned bitshift. */
+			return hash >>> 0;
+		}
+
+		if ( ! name) {
 			name = 'fT' + hashIt(window.self.document.title);
 		} else {
 			name = 'fT' + name;
 		}
+		/*ignore jslint end*/
+		/*jsl:end */
+		/* jshint ignore:end */
 
+		/* jshint ignore:start */
+		/*jsl:ignore* /
+		/*ignore jslint start*/
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -40,6 +49,8 @@
 		/*ignore jslint end*/
 		/*jsl:end */
 		/* jshint ignore:end */
+
+		return name;
 
 	};
 
