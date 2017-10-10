66da3999dcb1279a9d5a852dd86d1e0e5a8080c7
Daniel Vena <deaconvena+git@gmail.com>
2017-10-10 11:39:50 -0400

    updated for new init style

--- src/js/base.js
+++ src/js/base.js
@@ -1,57 +1,58 @@
-(function($, window, undefined){
-	
+/* jshint browser: true, devel: true */
+(function($, window, undefined) {
+
+	// Safe wrapper for console.log
+	function log(){
+		if (typeof console === 'object' && console.log) {
+			var ts = (new Date());
+			ts = ts.toISOString() ? ts.toISOString() : ts.toUTCString();
+			console.log('[CMLS Contesting]', ts, [].slice.call(arguments));
+		}
+	}
+
+	// Finds the content wrapper that contains the feature and adds
+	// classes to its column and row so we don't need to necessarily
+	// know the exact markup of the page.
+	var tag = $('#CMLS_FEATURE');
+	if (tag.length) {
+		tag.parentsUntil('.wrapper-content', '.column,.row,.block-type-content')
+			.addClass('CMLS_CCC');
+		log('Added CMLS_CCC class to parent containers.');
+	} else {
+		log('You must add id="CMLS_FEATURE" to the script tag which loads this library!');
+		return false;
+	}
+
+	window._CMLS = window._CMLS || {};
+
+	// If we have a google analytics ID, set it up.
+	var gaID = tag.attr('data-google-analytics-id');
+	if (gaID) {
+		log('Installing Google Analytics', gaID);
 		/**
-	 * Installs Google Analytics
-	 * @param  {string} id GA Property ID
-	 */
-	window.installGoogleAnalytics = function(id, name) {
-		if (! id) {
-			return;
-		}
-
-		/* jshint ignore:start */
-		/*jsl:ignore* /
-		/*ignore jslint start*/
-		/* adapted from https://github.com/darkskyapp/string-hash */
-		function hashIt(str) {
-			var hash = 5381,
-			i = str.length;
-
-			while(i) {
-				hash = (hash * 33) ^ str.charCodeAt(--i);
+		 * Installs GA with a given ID
+		 * @param  {string} id GA property ID
+		 * @return {void}
+		 */
+		window._CMLS.installGoogleAnalytics = function installGoogleAnalytics(id) {
+			if ( ! id) {
+				return;
 			}
+			/* jshint ignore:start */
+			/*jsl:ignore* /
+			/*ignore jslint start*/
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			/* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
-			* integers. Since we want the results to be always positive, convert the
-			* signed int to an unsigned by doing an unsigned bitshift. */
-			return hash >>> 0;
-		}
+			ga('create', id, 'auto', {'name': 'contestTracker'});
+			ga('contestTracker.send', 'pageview');
+			/*ignore jslint end*/
+			/*jsl:end */
+			/* jshint ignore:end */
+		};
+		window._CMLS.installGoogleAnalytics(gaID);
+	}
 
-		if ( ! name) {
-			name = 'fT' + hashIt(window.self.document.title);
-		} else {
-			name = 'fT' + name;
-		}
-		/*ignore jslint end*/
-		/*jsl:end */
-		/* jshint ignore:end */
-
-		/* jshint ignore:start */
-		/*jsl:ignore* /
-		/*ignore jslint start*/
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		ga('create', id, 'auto', {'name': name});
-		ga(name + '.send', 'pageview');
-		/*ignore jslint end*/
-		/*jsl:end */
-		/* jshint ignore:end */
-
-		return name;
-
-	};
-
-}(jQuery, window));
+}(jQuery, window.self));
