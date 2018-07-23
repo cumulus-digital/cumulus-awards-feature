b913badbe3c09c849605edcac89f022f884a92e1
Daniel Vena <deaconvena+github@gmail.com>
2018-07-23 18:41:48 -0400

    Only inject DFP for cube if cube placement is detected

--- src/iframe/inner/js/base.js
+++ src/iframe/inner/js/base.js
@@ -1,4 +1,42 @@
 /* jshint browser: true, devel: true */
+
+/* jshint ignore:start */
+/*jsl:ignore* /
+/*ignore jslint start*/
+
+// TWITTER TIMELINE WIDGET
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+// FACEBOOK API
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Google DFP Setup
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+(function() {
+	var gads = document.createElement('script');
+	gads.async = true;
+	gads.type = 'text/javascript';
+	var useSSL = 'https:' == document.location.protocol;
+	gads.src = (useSSL ? 'https:' : 'http:') + 
+	'//www.googletagservices.com/tag/js/gpt.js';
+	var node = document.getElementsByTagName('script')[0];
+	node.parentNode.insertBefore(gads, node);
+})();
+
+/*ignore jslint end*/
+/*jsl:end */
+/* jshint ignore:end */
+
+/**
+ * Application
+ */
 ;(function (window, undefined) {
 	
 	// Safe wrapper for console.log
@@ -43,6 +81,11 @@
 					window.self.parentIFrame.reset();
 				}
 			});
+
+			// Activate DFP if cube is present
+			if (window.self.document.getElementById('div-gpt-ad-1418849849333-0')) {
+				window.parent._CMLS.CCC_IFRAME_ACTIVATE_DFP(window.self);
+			}
 		});
 	};
 	window.document.head.appendChild(jqscr);
@@ -50,7 +93,7 @@
 	// Start up iframe-resizer
 	log('Injecting iframe-resizer contentWindow library');
 	var ifscr = window.document.createElement('script');
-	ifscr.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.contentWindow.min.js';
+	ifscr.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.1/iframeResizer.contentWindow.min.js';
 	ifscr.onload = function() {
 		log('iframe-resizer contentWindow loaded.');
 	};
--- src/iframe/outer/js/base.js
+++ src/iframe/outer/js/base.js
@@ -42,16 +42,13 @@
 				style: ""
 			});
 
-			window._CMLS.CCC_IFRAME_SETUP = function setupIframe() {
-				log('Inner frame called parent iframe setup');
+			window._CMLS.CCC_IFRAME_ACTIVATE_DFP = function setupDFP() {
 				var iwin = tag[0].contentWindow,
 					idoc = tag[0].contentDocument;
 
 				idoc.title = window.document.title;
 
 				// inject DFP setup
-				var googletags = [],
-					hasGoogletag = false;
 				$('script').each(function(){
 					if ( 
 						// don't bother with script tags that have a src
@@ -62,20 +59,29 @@
 							this.innerText.indexOf('googletag.pubads().setTargeting') > -1
 						)
 					) {
-						log('Activating parent DFP in iframe template', this);
-						if (this.innerText.indexOf('var googletag') > -1) {
-							googletags.unshift(this.innerText);
-							hasGoogletag = true;
-						} else {
-							googletags.push(this.innerText);
+						log('Retrieving DFP Property ID');
+						var idCheck = this.innerText.match(/\/6717\/([a-zA-Z\.\-]+)/);
+						if (idCheck.length > 1) {
+							log('Activating parent DFP in iframe template for cube', this);
+							iwin.eval(
+								'googletag.cmd.push(function() {' +
+								'	googletag.defineSlot(\'/6717/'+idCheck[1]+'\', [[300, 250], [300, 600]], \'div-gpt-ad-1418849849333-0\')' +
+								'		.addService(googletag.pubads())' +
+								'		.setCollapseEmptyDiv(true)' +
+								'		.setTargeting("pos","mid");' +
+								'	googletag.pubads().enableSingleRequest();' +
+								'	googletag.enableServices();' +
+								'});'
+							);
+							return false;
 						}
 					}
 				});
-				if (hasGoogletag) {
-					googletags.forEach(function(scr) {
-						iwin.eval(scr);
-					});
-				}
+			};
+
+			window._CMLS.CCC_IFRAME_SETUP = function setupIframe() {
+				log('Inner frame called parent iframe setup');
+				tag[0].contentDocument.title = window.document.title;
 			};
 
 			//tag.after(newframe);
@@ -86,7 +92,7 @@
 			// Set up iframe resizer
 			var ifscr = window.document.createElement('script'),
 				w = window;
-			ifscr.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.min.js';
+			ifscr.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.1/iframeResizer.min.js';
 			ifscr.type = 'text/javascript';
 			ifscr.onload = function(){
 				var isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1);
