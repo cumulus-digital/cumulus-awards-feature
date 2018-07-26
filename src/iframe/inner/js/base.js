865476a45145aee6dfea8bc13fb1771136c35bb7
Daniel Vena <deaconvena+github@gmail.com>
2018-07-26 00:17:02 -0400

    remove DFP activation code from inner

--- src/iframe/inner/js/base.js
+++ src/iframe/inner/js/base.js
@@ -16,20 +16,6 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-// Google DFP Setup
-var googletag = googletag || {};
-googletag.cmd = googletag.cmd || [];
-(function() {
-	var gads = document.createElement('script');
-	gads.async = true;
-	gads.type = 'text/javascript';
-	var useSSL = 'https:' == document.location.protocol;
-	gads.src = (useSSL ? 'https:' : 'http:') + 
-	'//www.googletagservices.com/tag/js/gpt.js';
-	var node = document.getElementsByTagName('script')[0];
-	node.parentNode.insertBefore(gads, node);
-})();
-
 /*ignore jslint end*/
 /*jsl:end */
 /* jshint ignore:end */
