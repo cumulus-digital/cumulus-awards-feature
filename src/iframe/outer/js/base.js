f25c073241378931cc3c61046bcab9b316f47d5a
Daniel Vena <deaconvena+github@gmail.com>
2018-07-26 00:12:58 -0400

    Use unshift to set up DFP before slot is called

--- src/iframe/outer/js/base.js
+++ src/iframe/outer/js/base.js
@@ -73,30 +73,35 @@
 							);
 						});
 					}
+
 					if (adPath) {
 						var dfpScript =
-							'var googletag = googletag || {};' +
-							'googletag.cmd = googletag.cmd || [];' +
-							'(function() {' +
-							'var gads = document.createElement(\'script\');' +
-							'gads.async = true;' +
-							'gads.type = \'text/javascript\';' +
-							'var useSSL = \'https:\' == document.location.protocol;' +
-							'gads.src = (useSSL ? \'https:\' : \'http:\') + ' +
-							'\'//www.googletagservices.com/tag/js/gpt.js\';' +
-							'var node = document.getElementsByTagName(\'script\')[0];' +
-							'node.parentNode.insertBefore(gads, node);' +
-							'})();' +
+							"googletag.cmd.unshift(function() {" +
+							"	" + targets.join("\n") +
+							"});" +
 
-							'googletag.cmd.push(function() {' +
-							'	' + targets.join("\n") +
-							'	googletag.defineSlot(\'' + adPath +'\', [[300, 250], [300, 600]], \'div-gpt-ad-1418849849333-0\')' +
-							'		.addService(googletag.pubads())' +
-							'		.setCollapseEmptyDiv(true)' +
-							'		.setTargeting("pos","mid");' +
-							'	googletag.pubads().enableSingleRequest();' +
-							'	googletag.enableServices();' +
-							'});';
+							"googletag.cmd.unshift(function() {" +
+							"	googletag.defineSlot('" + adPath + "', [[300, 250], [300, 600]], 'div-gpt-ad-1418849849333-0')" +
+							"		.addService(googletag.pubads())" +
+							"		.setCollapseEmptyDiv(true)" +
+							"		.setTargeting('pos','mid');" +
+							"	googletag.pubads().enableSingleRequest();" +
+							"	googletag.enableServices();" +
+							"});" +
+
+							"var googletag = googletag || {};" +
+							"googletag.cmd = googletag.cmd || [];" +
+							"(function() {" +
+							"var gads = document.createElement('script');" +
+							"gads.async = true;" +
+							"gads.type = 'text/javascript';" +
+							"var useSSL = 'https:' == document.location.protocol;" +
+							"gads.src = (useSSL ? 'https:' : 'http:') + " +
+							"'//www.googletagservices.com/tag/js/gpt.js';" +
+							"var node = document.getElementsByTagName('script')[0];" +
+							"node.parentNode.insertBefore(gads, node);" +
+							"})();";
+
 						log('Activating parent DFP in iframe template for cube', dfpScript);
 						iwin.eval(dfpScript);
 					}
