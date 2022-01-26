824f0b8366d0ab1cb3a4565b575caea7566fdf06
Daniel Vena <deaconvena@gmail.com>
2022-01-26 17:37:10 -0500

    polyfilling and DRY

--- src/iframe/inner/js/base.js
+++ src/iframe/inner/js/base.js
@@ -24,8 +24,6 @@
  * Application
  */
 (function (window, undefined) {
-
-	var Promise = window.Promise;
 	
 	// Safe wrapper for console.log
 	function log(){
@@ -36,262 +34,271 @@
 		}
 	}
 
-	// For any fake iframes
-	window.document.createElement('iiframe'); // jshint ignore:line
-
-	// Throttle from Underscore.js
-	var _throttle = function (func, wait) {
-		var context, args, result;
-		var timeout = null;
-		var previous = 0;
-
-		var later = function () {
-			previous = (new Date()).getTime();
-			timeout = null;
-			result = func.apply(context, args);
-			if (!timeout) { context = args = null; }
+	// Polyfill promise if necessary
+	function loadScript(src, done) {
+		var js = window.self.document.createElement('script');
+		js.src = src;
+		js.onload = function() {
+			done();
 		};
-		return function () {
-			var now = (new Date()).getTime();
-			if (!previous) { previous = now; }
-			var remaining = wait - (now - previous);
-			context = this;
-			args = arguments;
-			if (remaining <= 0 || remaining > wait) {
-				if (timeout) {
-					clearTimeout(timeout);
-					timeout = null;
-				}
-				previous = now;
+		js.onerror = function() {
+			done(new Error('Failed to load script ' + src));
+		};
+		window.self.document.head.appendChild(js);
+	}
+	if (window.self.Promise) {
+		main();
+	} else {
+		loadScript('https://polyfill.io/v3/polyfill.min.js?features=Promise', main);
+	}
+
+	function main() {
+
+		var Promise = window.Promise;
+
+		// For any fake iframes
+		window.document.createElement('iiframe'); // jshint ignore:line
+
+		// Throttle from Underscore.js
+		var _throttle = function (func, wait) {
+			var context, args, result;
+			var timeout = null;
+			var previous = 0;
+
+			var later = function () {
+				previous = (new Date()).getTime();
+				timeout = null;
 				result = func.apply(context, args);
 				if (!timeout) { context = args = null; }
-			} else if (!timeout) {
-				timeout = setTimeout(later, remaining);
-			}
-			return result;
+			};
+			return function () {
+				var now = (new Date()).getTime();
+				if (!previous) { previous = now; }
+				var remaining = wait - (now - previous);
+				context = this;
+				args = arguments;
+				if (remaining <= 0 || remaining > wait) {
+					if (timeout) {
+						clearTimeout(timeout);
+						timeout = null;
+					}
+					previous = now;
+					result = func.apply(context, args);
+					if (!timeout) { context = args = null; }
+				} else if (!timeout) {
+					timeout = setTimeout(later, remaining);
+				}
+				return result;
+			};
 		};
-	};
 
-	// Handle lazyload
-	var _lazyload = {
-		selector: '[lazyload="on"]',
-		advance: 200,
-		getLoadable: function() {
-			if ( ! this.loadable) {
-				this.loadable = window.$(this.selector);
-			}
-			return this.loadable;
-		},
-		handler: function() {
-			_lazyload.getLoadable().each(function() {
-				if (this.dataset.originalsrc && this.src === this.dataset.originalsrc) {
-					return;
+		// Handle lazyload
+		var _lazyload = {
+			selector: '[lazyload="on"]',
+			advance: 200,
+			getLoadable: function() {
+				if ( ! this.loadable) {
+					this.loadable = window.$(this.selector);
 				}
-				var style = getComputedStyle(this),
-					bounds = this.getBoundingClientRect(),
-					frameBounds = this.ownerDocument.defaultView.frameElement.getBoundingClientRect();
-				if (
-					(bounds && frameBounds) &&
-					bounds.top + frameBounds.top - _lazyload.advance <= window.self.parent.innerHeight &&
-					style.display !== 'none'
-				) {
-					if (this.dataset.src) {
-						if ( ! this.dataset.original_src) {
-							this.dataset.original_src = this.src;
-						}
-						if (this.src.indexOf(this.dataset.src) === -1) {
-							log('Setting src', this, this.dataset.src, this.src);
-							this.src = this.dataset.src;
-						}
-						if (this.dataset.srcset) {
-							if ( ! this.dataset.original_srcset) {
-								this.dataset.original_srcset = this.srcset;
-							}
-							if (this.srcset !== this.dataset.srcset) {
-								this.srcset = this.dataset.srcset;
-							}
-						}
-					}
-				}
-			});
-		}
-	};
-
-	// jQuery loader promise
-	window.jQueryLoader = function(){
-		return new Promise(function(resolve, reject) {
-			if ( ! window.hasOwnProperty('jQuery')) {
-				var jqScr = document.createElement('script');
-				jqScr.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
-				jqScr.type = 'text/javascript';
-				jqScr.onload = function() {
-					log('jQuery injected.');
-					var $ = window.jQuery;
-					window.$ = $;
-					var loadedEvent = window.self.document.createEvent('Event');
-					loadedEvent.initEvent('jquery.loaded', true, true);
-					window.self.dispatchEvent(loadedEvent);
-					resolve();
-				}
-				jqScr.onerror = function(){
-					reject(new Error('Failed to load jQuery!'));
-				};
-				document.head.appendChild(jqScr);
-			} else {
-				resolve();
-			}
-		});
-	};
-	window.jQueryLoader().then(function(){
-		var $ = window.jQuery;
-		$(function(){
-
-			// Handle our fake iframes
-			$('iiframe').each(function() {
-				var $container = $(this),
-					$newframe = $('<iframe/>'),
-					attrs = $container.prop('attributes');
-				log('Resolving inner iframe', this, attrs);
-				$.each(attrs, function(){
-					if (this.specified) {
-						$newframe.attr(this.name, this.value);
-					}
-				});
-				$container.after($newframe);
-				Object.assign($newframe[0].dataset, $container[0].dataset);
-				$container.remove();
-			});
-
-			// Load events make parent iframe resize
-			$('img,iframe').on('load', function() {
-				if (window.self.parentIFrame) {
-					window.self.parentIFrame.reset();
-				}
-			});
-
-			// Activate LazyLoader
-			window.self.parent.document.addEventListener("scroll", _throttle(_lazyload.handler, 500));
-			window.self.parent.addEventListener("resize", _throttle(_lazyload.handler, 500));
-			window.self.parent.addEventListener("orientationchange", _throttle(_lazyload.handler, 500));
-
-			_lazyload.handler();
-
-		});
-	});
-
-	window.ifrLoader = function(){
-		return new Promise(function(resolve, reject) {
-			if ( ! window.hasOwnProperty('parentIFrame')) {
-				log('Injecting iframe-resizer contentWindow library');
-				var ifrScr = document.createElement('script');
-				ifrScr.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.contentWindow.min.js';
-				ifrScr.type = 'text/javascript';
-				ifrScr.onload = function() {
-					log('iframe-resizer contentWindow loaded.');
-					var loadedEvent = window.self.document.createEvent('Event');
-					loadedEvent.initEvent('ifr.loaded', true, true);
-					window.self.dispatchEvent(loadedEvent);
-					resolve();
-				}
-				document.head.appendChild(ifrScr);
-			} else {
-				resolve();
-			}
-		});
-	};
-	window.ifrLoader();
-
-	// Handle DFP
-	function waitForParentDFP() {
-		var timeout = 10000, // 10 seconds,
-		    start = Date.now();
-		function waitingForParentDFP(resolve, reject) {
-			if (window.self.parent.googletag && window.self.parent.googletag.pubads) {
-				resolve(window.self.parent.googletag);
-			} else if (timeout && (Date.now() - start) >= timeout) {
-				reject(new Error('timeout'));
-			} else {
-				setTimeout(waitingForParentDFP.bind(this, resolve, reject), 50);
-			}
-		}
-		return new Promise(waitingForParentDFP);
-	}
-	window.self.INIT_DFP = function INIT_DFP(sizes) {
-
-		log('DFP init requested, waiting for parent googletag...');
-		waitForParentDFP()
-		.then(
-			function(){
-
-				var googletag = window.self.googletag || {cmd: []},
-				    wp = window.self.parent,
-				    g = wp.googletag,
-				    gpa = g.pubads,
-				    adPath = null,
-				    targetingKeys = null;
-
-				// Discover ad path of parent site
-				if (wp.GPT_SITE_ID) {
-					adPath = wp.GPT_SITE_ID;
-				} else {
-					var slots = gpa().getSlots();
-					if (slots.length) {
-						slots.some(function(slot) {
-							var p = slot.getAdUnitPath();
-							if (p.indexOf('/6717') > -1) {
-								adPath = p;
-								return true;
-							}
-						});
-					}
-				}
-
-				// Make sure we have an adpath
-				if ( ! adPath) {
-					log('Could not determine parent adPath, exiting DFP activation');
-					return;
-				}
-
-				// Find existing global targeting keys from parent window
-				targetingKeys = gpa().getTargetingKeys();
-				if (targetingKeys && targetingKeys.length) {
-					log('Setting DFP targeting keys', targetingKeys);
-					googletag.cmd.unshift(function defineTargets() {
-						targetingKeys.forEach(function(key) {
-							var t = gpa().getTargeting(key);
-							log('Defining GPT target', key, t);
-							googletag.pubads().setTargeting(key, t);
-						});
-					});
-				}
-
-				googletag.cmd.unshift(function(){
-					log('Setting up DFP slot');
-					var slot = googletag.defineSlot(adPath, sizes, 'div-gpt-cube')
-					           .addService(googletag.pubads())
-					           .setCollapseEmptyDiv(true)
-					           .setTargeting('pos', 'mid');
-					googletag.pubads().enableSingleRequest();
-					googletag.enableServices();
-				});
-
-				(function() {
-					var gads = window.self.document.createElement('script');
-					gads.async = true;
-					gads.type = 'text/javascript';
-					gads.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
-					var node = window.self.document.getElementsByTagName('script')[0];
-					node.parentNode.insertBefore(gads, node);
-				})();
-
-				log('DFP activated.');
+				return this.loadable;
 			},
-			function(error){
-				log('Timed out waiting for parent googletag.');
+			handler: function() {
+				_lazyload.getLoadable().each(function() {
+					if (this.dataset.originalsrc && this.src === this.dataset.originalsrc) {
+						return;
+					}
+					var style = getComputedStyle(this),
+						bounds = this.getBoundingClientRect(),
+						frameBounds = this.ownerDocument.defaultView.frameElement.getBoundingClientRect();
+					if (
+						(bounds && frameBounds) &&
+						bounds.top + frameBounds.top - _lazyload.advance <= window.self.parent.innerHeight &&
+						style.display !== 'none'
+					) {
+						if (this.dataset.src) {
+							if ( ! this.dataset.original_src) {
+								this.dataset.original_src = this.src;
+							}
+							if (this.src.indexOf(this.dataset.src) === -1) {
+								log('Setting src', this, this.dataset.src, this.src);
+								this.src = this.dataset.src;
+							}
+							if (this.dataset.srcset) {
+								if ( ! this.dataset.original_srcset) {
+									this.dataset.original_srcset = this.srcset;
+								}
+								if (this.srcset !== this.dataset.srcset) {
+									this.srcset = this.dataset.srcset;
+								}
+							}
+						}
+					}
+				});
 			}
-		);
+		};
 
-	};
+		// jQuery loader promise
+		window.jQueryLoader = function(){
+			return new Promise(function(resolve, reject) {
+				if ( ! window.hasOwnProperty('jQuery')) {
+					loadScript('https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', function() {
+						var loadedEvent = window.self.document.createEvent('Event');
+						loadedEvent.initEvent('jquery.loaded', true, true);
+						window.self.dispatchEvent(loadedEvent);
+						resolve();
+					});
+				} else {
+					resolve();
+				}
+			});
+		};
+		window.jQueryLoader().then(function(){
+			var $ = window.jQuery;
+			$(function(){
+
+				// Handle our fake iframes
+				$('iiframe').each(function() {
+					var $container = $(this),
+						$newframe = $('<iframe/>'),
+						attrs = $container.prop('attributes');
+					log('Resolving inner iframe', this, attrs);
+					$.each(attrs, function(){
+						if (this.specified) {
+							$newframe.attr(this.name, this.value);
+						}
+					});
+					$container.after($newframe);
+					Object.assign($newframe[0].dataset, $container[0].dataset);
+					$container.remove();
+				});
+
+				// Load events make parent iframe resize
+				$('img,iframe').on('load', function() {
+					if (window.self.parentIFrame) {
+						window.self.parentIFrame.reset();
+					}
+				});
+
+				// Activate LazyLoader
+				window.self.parent.document.addEventListener("scroll", _throttle(_lazyload.handler, 500));
+				window.self.parent.addEventListener("resize", _throttle(_lazyload.handler, 500));
+				window.self.parent.addEventListener("orientationchange", _throttle(_lazyload.handler, 500));
+
+				_lazyload.handler();
+
+			});
+		});
+
+		window.ifrLoader = function(){
+			return new Promise(function(resolve, reject) {
+				if ( ! window.hasOwnProperty('parentIFrame')) {
+					log('Injecting iframe-resizer contentWindow library');
+					loadScript('https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.contentWindow.min.js', function(e) {
+						var loadedEvent = window.self.document.createEvent('Event');
+						loadedEvent.initEvent('ifr.loaded', true, true);
+						window.self.dispatchEvent(loadedEvent);
+						resolve();
+					});
+				} else {
+					resolve();
+				}
+			});
+		};
+		window.ifrLoader();
+
+		// Handle DFP
+		function waitForParentDFP() {
+			var timeout = 10000, // 10 seconds,
+			    start = Date.now();
+			function waitingForParentDFP(resolve, reject) {
+				if (window.self.parent.googletag && window.self.parent.googletag.pubads) {
+					resolve(window.self.parent.googletag);
+				} else if (timeout && (Date.now() - start) >= timeout) {
+					reject(new Error('timeout'));
+				} else {
+					setTimeout(waitingForParentDFP.bind(this, resolve, reject), 50);
+				}
+			}
+			return new Promise(waitingForParentDFP);
+		}
+		window.self.INIT_DFP = function INIT_DFP(sizes) {
+
+			log('DFP init requested, waiting for parent googletag...');
+			waitForParentDFP()
+			.then(
+				function(){
+
+					var googletag = window.self.googletag || {cmd: []},
+					    wp = window.self.parent,
+					    g = wp.googletag,
+					    gpa = g.pubads,
+					    adPath = null,
+					    targetingKeys = null;
+
+					// Discover ad path of parent site
+					if (wp.GPT_SITE_ID) {
+						adPath = wp.GPT_SITE_ID;
+					} else {
+						var slots = gpa().getSlots();
+						if (slots.length) {
+							slots.some(function(slot) {
+								var p = slot.getAdUnitPath();
+								if (p.indexOf('/6717') > -1) {
+									adPath = p;
+									return true;
+								}
+							});
+						}
+					}
+
+					// Make sure we have an adpath
+					if ( ! adPath) {
+						log('Could not determine parent adPath, exiting DFP activation');
+						return;
+					}
+
+					// Find existing global targeting keys from parent window
+					targetingKeys = gpa().getTargetingKeys();
+					if (targetingKeys && targetingKeys.length) {
+						log('Setting DFP targeting keys', targetingKeys);
+						googletag.cmd.unshift(function defineTargets() {
+							targetingKeys.forEach(function(key) {
+								var t = gpa().getTargeting(key);
+								log('Defining GPT target', key, t);
+								googletag.pubads().setTargeting(key, t);
+							});
+						});
+					}
+
+					googletag.cmd.unshift(function(){
+						log('Setting up DFP slot');
+						var slot = googletag.defineSlot(adPath, sizes, 'div-gpt-cube')
+						           .addService(googletag.pubads())
+						           .setCollapseEmptyDiv(true)
+						           .setTargeting('pos', 'mid');
+						googletag.pubads().enableSingleRequest();
+						googletag.enableServices();
+					});
+
+					(function() {
+						var gads = window.self.document.createElement('script');
+						gads.async = true;
+						gads.type = 'text/javascript';
+						gads.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
+						var node = window.self.document.getElementsByTagName('script')[0];
+						node.parentNode.insertBefore(gads, node);
+					})();
+
+					log('DFP activated.');
+				},
+				function(error){
+					log('Timed out waiting for parent googletag.');
+				}
+			);
+
+		};
+
+	}
 
 	log('Setting document title from parent');
 	window.self.document.title = window.self.parent.document.title;
--- src/iframe/outer/js/base.js
+++ src/iframe/outer/js/base.js
@@ -6,6 +6,9 @@
 (function($, window, undefined){
 
 	window.self._CMLS = window.self._CMLS || {};
+	var frame_id = 'CMLS_CCC_IFRAME',
+		frame_parent = window.self;
+		frame_parent._CMLS = frame_parent._CMLS || {};
 
 	// Safe wrapper for console.log
 	function log(){
@@ -16,106 +19,115 @@
 		}
 	}
 
-	var frame_id = 'CMLS_CCC_IFRAME',
-		frame_parent = window.self;
-		frame_parent._CMLS = frame_parent._CMLS || {};
-
-	// Add title to interior frame from container site
-	frame_parent._CMLS.CCC_IFRAME_SETUP_COUNT = 0;
-	frame_parent._CMLS.CCC_IFRAME_SETUP = function CCC_IFRAME_SETUP() {
-		var $frame = $('#' + frame_id, frame_parent.document);
-
-		if ( ! $frame.length) {
-			log('Could not find generated frame when attempting to setup iframe');
-			if (frame_parent._CMLS.CCC_IFRAME_SETUP_COUNT < 80) {
-				log('Trying again...');
-				frame_parent._CMLS.CCC_IFRAME_SETUP_COUNT++;
-				setTimeout(function(){
-					frame_parent._CMLS.CCC_IFRAME_SETUP();
-				}, 200);
-			}
-			return;
-		}
-
-		log('Setting title on generated frame document');
-		$frame.each(function() {
-			this.contentDocument.title = frame_parent.document.title;
-		});
-	};
-
-	$(function(){
-
-		// Check for required identification tag of iframe
-		var $tag = $('#CMLS_TEMPLATE:first', frame_parent.document);
-		if ( ! $tag.length) {
-			log('No #CMLS_TEMPLATE found, not an iframe feature');
-			return;
-		}
-
-		// Ensure tag is an iframe
-		if ( ! $tag.is('iframe')) {
-			log('#CMLS_TEMPLATE is not an iframe feature!');
-			return;
-		}
-
-		// Wrap our content in a recognizable class
-		$tag.parentsUntil('.wrapper-content', '.column,.row,.block-type-content')
-			.addClass('CMLS_CCC');
-		
-		// Generate new iframe
-		var $new_frame = $('<iframe/>', {
-			id: frame_id,
-			name: frame_id,
-			frameBorder: 0,
-			width: '100%',
-			class: 'CMLS_CCC_IFRAME',
-			scrolling: false,
-			allowTransparency: true,
-			style: 'width: 100%',
-		});
-		
-		// Write contents of template into generated iframe
-		var template_content = $tag.text();
-		if ( !!('srcdoc' in document.createElement('iframe'))) {
-			$new_frame[0].srcdoc = template_content;
-			$new_frame.insertBefore($tag);
-		} else {
-			$new_frame.insertBefore($tag);
-			var new_frame_doc = $new_frame[0].contentDocument;
-			new_frame_doc.open();
-			new_frame_doc.write(template_content);
-			new_frame_doc.close();
-		}
-
-		$tag.detach();
-
-		// Inject iFrameResizer into parent window
-		var ifscr = frame_parent.document.createElement('script'),
-			hasTaggedElement = (template_content.indexOf('data-iframe-height') > -1) ? true : false;
-		ifscr.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js';
-		ifscr.type = 'text/javascript';
-		ifscr.onload = function(){
-			var isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1);
-			if (hasTaggedElement) {
-				log('Template content has tagged element for IFR');
-			}
-			frame_parent.iFrameResize({
-				//log: window.IFR_DEBUG || false,
-				log: (frame_parent._CMLS && frame_parent._CMLS.debug) || frame_parent.IFR_DEBUG ? true : false,
-				checkOrigin: false,
-				sizeWidth: false,
-				tolerance: 10,
-				minSize: 100,
-				heightCalculationMethod: hasTaggedElement ? 'taggedElement' : isOldIE ? 'max' : 'bodyOffset',
-				onInit: function(ifr) {
-					var $ifr = $(ifr);
-					$ifr.attr('init', true);
-					$ifr.trigger('cmls-ifr-init');
-					$(frame_parent).trigger('cmls-ifr-init');
-				}
-			}, '#' + frame_id);
+	function loadScript(src, done) {
+		var js = frame_parent.document.createElement('script');
+		js.src = src;
+		js.onload = function() {
+			done();
 		};
-		frame_parent.document.head.appendChild(ifscr);
+		js.onerror = function() {
+			done(new Error('Failed to load script ' + src));
+		};
+		frame_parent.document.head.appendChild(js);
+	}
 
-	}, frame_parent.document);
+	main();
+
+	function main() {
+
+		// Add title to interior frame from container site
+		frame_parent._CMLS.CCC_IFRAME_SETUP_COUNT = 0;
+		frame_parent._CMLS.CCC_IFRAME_SETUP = function CCC_IFRAME_SETUP() {
+			var $frame = $('#' + frame_id, frame_parent.document);
+
+			if ( ! $frame.length) {
+				log('Could not find generated frame when attempting to setup iframe');
+				if (frame_parent._CMLS.CCC_IFRAME_SETUP_COUNT < 80) {
+					log('Trying again...');
+					frame_parent._CMLS.CCC_IFRAME_SETUP_COUNT++;
+					setTimeout(function(){
+						frame_parent._CMLS.CCC_IFRAME_SETUP();
+					}, 200);
+				}
+				return;
+			}
+
+			log('Setting title on generated frame document');
+			$frame.each(function() {
+				this.contentDocument.title = frame_parent.document.title;
+			});
+		};
+
+		$(function(){
+
+			// Check for required identification tag of iframe
+			var $tag = $('#CMLS_TEMPLATE:first', frame_parent.document);
+			if ( ! $tag.length) {
+				log('No #CMLS_TEMPLATE found, not an iframe feature');
+				return;
+			}
+
+			// Ensure tag is an iframe
+			if ( ! $tag.is('iframe')) {
+				log('#CMLS_TEMPLATE is not an iframe feature!');
+				return;
+			}
+
+			// Wrap our content in a recognizable class
+			$tag.parentsUntil('.wrapper-content', '.column,.row,.block-type-content')
+				.addClass('CMLS_CCC');
+			
+			// Generate new iframe
+			var $new_frame = $('<iframe/>', {
+				id: frame_id,
+				name: frame_id,
+				frameBorder: 0,
+				width: '100%',
+				class: 'CMLS_CCC_IFRAME',
+				scrolling: false,
+				allowTransparency: true,
+				style: 'width: 100%',
+			});
+			
+			// Write contents of template into generated iframe
+			var template_content = $tag.text();
+			if ( !!('srcdoc' in document.createElement('iframe'))) {
+				$new_frame[0].srcdoc = template_content;
+				$new_frame.insertBefore($tag);
+			} else {
+				$new_frame.insertBefore($tag);
+				var new_frame_doc = $new_frame[0].contentDocument;
+				new_frame_doc.open();
+				new_frame_doc.write(template_content);
+				new_frame_doc.close();
+			}
+
+			$tag.detach();
+
+			// Inject iFrameResizer into parent window
+			var hasTaggedElement = (template_content.indexOf('data-iframe-height') > -1) ? true : false;
+			loadScript('https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js', function() {
+				var isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1);
+				if (hasTaggedElement) {
+					log('Template content has tagged element for IFR');
+				}
+				frame_parent.iFrameResize({
+					//log: window.IFR_DEBUG || false,
+					log: (frame_parent._CMLS && frame_parent._CMLS.debug) || frame_parent.IFR_DEBUG ? true : false,
+					checkOrigin: false,
+					sizeWidth: false,
+					tolerance: 10,
+					minSize: 100,
+					heightCalculationMethod: hasTaggedElement ? 'taggedElement' : isOldIE ? 'max' : 'bodyOffset',
+					onInit: function(ifr) {
+						var $ifr = $(ifr);
+						$ifr.attr('init', true);
+						$ifr.trigger('cmls-ifr-init');
+						$(frame_parent).trigger('cmls-ifr-init');
+					}
+				}, '#' + frame_id);
+			});
+
+		}, frame_parent.document);
+	}
 }(window.jQuery, window.self));
